bb6a18fc123b480fac3853647246d1d9db51dc52
MSI <adi.permana60@ymail.com>
2018-03-03 17:32:32 +0700

    mantap

---
+++ app/src/main/java/com/example/juli_soep/sekolah/BAK/ShowGajiStaff.java
@@ -0,0 +1,15 @@
+package com.example.juli_soep.sekolah.BAK;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.juli_soep.sekolah.R;
+
+public class ShowGajiStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_gaji_staff);
+    }
+}
